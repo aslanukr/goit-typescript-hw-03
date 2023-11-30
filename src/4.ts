00c675be15f7923e765009bf7d3861d82ccfbf3b
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-12-01 01:59:28 +0200

    completes task

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,45 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {}
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door = false;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Welcome home!");
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(myKey: Key): void {
+    if (this.key.getSignature() === myKey.getSignature()) {
+      this.door = true;
+      console.log("Home sweet home!");
+    } else {
+      console.log("BURGLAR!!!!");
+    }
+  }
+}
+
+const key = new Key(Math.random());
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +47,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
